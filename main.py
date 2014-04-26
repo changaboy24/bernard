fa50c58c4696159233b10b6689f9bf4a7e9490ed
Rich Chang <changaboy24@gmail.com>
2014-04-26 17:37:37 -0400

    got categories and homepage checkbox working

---
+++ main.py
@@ -0,0 +1,80 @@
+from datetime import datetime
+from flask import Flask, request, flash, url_for, redirect, \
+     render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+db = SQLAlchemy(app)
+
+
+class Photo(db.Model):
+    __tablename__ = 'photos'
+    id = db.Column('photo_id', db.Integer, primary_key=True)
+    caption = db.Column(db.Text)
+    date = db.Column(db.DateTime)
+    category = db.Column(db.String(80))
+    url = db.Column(db.Text)
+    homepage = db.Column(db.Boolean)
+
+    def __init__(self, caption, category, url, homepage):
+        self.caption = caption
+        self.category = category
+        self.url = url
+        self.date = datetime.now()
+        self.homepage = homepage
+
+
+@app.route('/')
+def show_all():
+    categories = get_categories()
+    photo_dict = {}
+    for category in categories:
+        photos = Photo.query.filter_by(category = category)
+        photo_dict[category] = photos
+
+    print photo_dict
+    return render_template('show_all.html',photo_dict = photo_dict)
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['caption']:
+            flash('caption is required', 'error')
+        elif not request.form['url']:
+            flash('url is required', 'error')
+        elif not request.form['category']:
+            flash('category is required', 'error')
+        else:
+            homepage = True
+            if request.form.get('homepage') == None:
+                homepage = False
+
+            photo = Photo(request.form['caption'], request.form['category'], request.form['url'], homepage)
+            db.session.add(photo)
+            db.session.commit()
+            flash('Photo was successfully created')
+            return redirect(url_for('show_all'))
+    return render_template('new.html')
+
+@app.route("/delete/<id>")
+def delete(id):
+    photo = Photo.query(filter_by(id = id).get(1))
+    db.session.delete(photo)
+    db.session.commit()
+    return render_template('show_all.html')
+
+##helper functions
+
+def get_categories():
+    photos = Photo.query.all()
+    categories = []
+    for photo in photos:
+        if photo.category not in categories:
+            categories.append(photo.category)
+    return categories
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
--- schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists pictures;
-create table pictures (
-  id integer primary key autoincrement,
-  title text not null,
-  caption text,
-  category text,
-  url text,
-  date date
-);
